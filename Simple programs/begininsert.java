80e4807b56cd9c210e36e5dc7161061de9890613
srikrishna213 <146162182+srikrishna213@users.noreply.github.com>
2024-07-26 12:03:29 +0530

    Add files via upload

---
+++ Simple programs/begininsert.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+class SinglyCircular{
+    Node head;
+    SinglyCircular() {
+        head = null;
+    }
+    void insertbeg(int data) {
+        Node nn = new Node(data);
+        if (head == null) {
+            nn.next=nn;
+            head = nn;
+        } else {
+            Node temp=head;
+            while(temp.next!=head)
+            {
+                temp=temp.next;
+            }
+            temp.next=nn;
+            nn.next=head;
+            head=nn;
+        }
+    }
+    void display() 
+    {
+        Node temp = head;
+        do
+        {
+            System.out.printf("%d ",temp.data);
+            temp = temp.next;
+        }while(temp != head);
+    }
+}
+class begininsert 
+{
+    public static void main(String [] args)
+    {
+        Scanner ob=new Scanner(System.in);
+        SinglyCircular s=new SinglyCircular();
+        int val=0;
+        while(val!=-1)
+        {
+            val=ob.nextInt();
+            if(val==-1)break;
+            s.insertbeg(val);
+        }
+        s.display();
+    }
+}
